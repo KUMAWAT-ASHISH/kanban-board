626632118cc96c3632383d5d7738b53c42705be8
ashish kumawat <legendarmy856@gmail.com>
2024-09-13 00:05:21 +0530

    Initial Commit

---
+++ src/interfaces.js
@@ -0,0 +1,27 @@
+// Ticket interface
+export const Ticket = {
+    id: '',
+    title: '',
+    tag: [],
+    userId: '',
+    status: '',
+    priority: 0
+  };
+  
+  // User interface
+  export const User = {
+    id: '',
+    name: '',
+    available: false
+  };
+  
+  // Col interface
+  export const Col = {
+    col: []
+  };
+  
+  // UserIdToData interface
+  export const UserIdToData = {
+    userData: {}
+  };
+  
---
+++ src/reportWebVitals.jsx
@@ -0,0 +1,19 @@
+// reportWebVitals.js
+
+// This function logs or sends web vitals to an analytics endpoint
+// You can modify it to suit your needs
+
+const reportWebVitals = (onPerfEntry) => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+      import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      });
+    }
+  };
+  
+  export default reportWebVitals;
+  
